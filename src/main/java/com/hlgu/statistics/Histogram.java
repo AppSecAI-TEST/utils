843fb0d0011597f7d499943388d201ce71ce4f78
Gu Hailin <linhaigu@gmail.com>
2017-08-11 14:52:43 +0800

    add new function to histogram

--- src/main/java/com/hlgu/statistics/Histogram.java
+++ src/main/java/com/hlgu/statistics/Histogram.java
@@ -1,7 +1,7 @@
 package com.hlgu.statistics;
 
 import com.google.common.collect.Maps;
-import com.hlgu.tools.Tools;
+import static com.hlgu.tools.Tools.*;
 
 import java.util.List;
 import java.util.Map;
@@ -12,7 +12,22 @@ public final class Histogram {
     public static <K> Map<K, Integer> histo(List<K> list){
         Map<K, Integer> histogram = Maps.newHashMap();
         for (K record : list)
-            Tools.add2Map(histogram, record, 1);
+            add2Map(histogram, record, 1);
         return histogram;
+    }
+
+    public static Map<String, Double> percentageHisto(String in, String delimiter){
+        Map<String, Double> map = Maps.newHashMap();
+        String[] records = in.split(delimiter);
+        Map<String, Integer> histogram = Maps.newHashMap();
+        double sum = 0.0d;
+        for (String record : records) {
+            add2Map(histogram, record, 1);
+            sum += 1;
+        }
+
+        for (String record : records)
+            map.put(record, mapGetOrElse(histogram, record, 0) / sum);
+        return map;
     }
 }
